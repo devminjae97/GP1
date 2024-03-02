3e643df4feb80cb551ee0fa638743418a26a872d
Minjae_Cho <t9100@naver.com>
2024-03-02 22:30:04 +0900

    [GP1-3] Inventory/Item 구성 요소 준비

---
+++ GP1/Assets/Scripts/Item/ItemInstance.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInstance : MonoBehaviour//, IEarnable 먹을 수 있는, IPlaceable 배치할 수 있는 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GP1/Assets/Scripts/UI/InventoryManager.cs
+++ GP1/Assets/Scripts/UI/InventoryManager.cs
@@ -1,18 +1,53 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryManager : MonoBehaviour
+/**
+ * 24.03.02.
+ * @조민재, 최초 작성
+ */
+
+public class InventoryManager : Singleton<InventoryManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _drawer = null;
+
+    private bool _opened = false;
+ 
+    //private List<ItemInstance> Grid;
+    
+    public void ToggleInventory()
+        => OpenInventory(!_opened);   
+    
+    public void OpenInventory(bool open)
     {
-        
+        _opened = open;
+        _drawer.SetActive(open);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddItem()
     {
         
     }
+    
+    public void RemoveItem()
+    {
+        
+    }
+    
+    public void UseItem()
+    {
+        
+    }
+    
+    #region #### Unity
+
+    private void Awake()
+    {
+        if (_drawer == null)
+        {
+            EditorLog.Error($"[{nameof(InventoryManager)}] {nameof(_drawer)} is missing.");
+        }
+    }
+
+    #endregion
 }
